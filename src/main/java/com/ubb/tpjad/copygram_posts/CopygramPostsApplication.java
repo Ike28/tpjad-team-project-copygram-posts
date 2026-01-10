22e87110684f4af2190050a96abd3566e38069b0
Ike28 <mihaijrcorocaescu@gmail.com>
2026-01-10 21:39:54 +0200

    init

---
+++ src/main/java/com/ubb/tpjad/copygram_posts/CopygramPostsApplication.java
@@ -0,0 +1,13 @@
+package com.ubb.tpjad.copygram_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopygramPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopygramPostsApplication.class, args);
+	}
+
+}
