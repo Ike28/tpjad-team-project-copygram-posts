c59d712c98adc5bb0d285ba0fc3706a52e504ed3
Ike28 <mihaijrcorocaescu@gmail.com>
2026-01-11 20:56:55 +0200

    Minor fix

--- src/main/java/com/ubb/tpjad/copygram_posts/api/exception/InvalidLikeActionException.java
+++ src/main/java/com/ubb/tpjad/copygram_posts/api/exception/InvalidLikeActionException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class InvalidLikeActionException extends RuntimeException {
     public static final String POST_ENTITY = "POST";
-    public static final String COMMENT_ENTITY = "POST";
+    public static final String COMMENT_ENTITY = "COMMENT";
 
     private static final String DUPLICATE_LIKE_MESSAGE_TEMPLATE = "Duplicate like request for entity %s of type %s from user %s";
     private static final String INVALID_UNLIKE_MESSAGE_TEMPLATE = "Invalid unlike request for entity %s of type %s from user %s";
